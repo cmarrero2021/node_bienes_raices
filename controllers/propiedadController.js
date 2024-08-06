7a0388cdcb29147ac5479ed18e2788de9849a93d
cmarrero2021 <marrero.c@gmail.com>
2024-08-06 18:46:08 -0400

    20240806_18444 Se completó el login"

---
+++ controllers/propiedadController.js
@@ -0,0 +1,8 @@
+const admin = (req,res) => {
+    res.render('./propiesdades/admin',{
+        pagina:'Mis Propiedades'
+    })
+}
+export {
+    admin
+}
--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import csrf from 'csurf';
 import cookieParser from 'cookie-parser';
 import usuarioRoutes from './routes/usuarioRoutes.js';
+import propiedadesRoutes from './routes/propiedadesRoutes.js';
 import db from './config/db.js';
 const app = express();
 app.use(express.urlencoded({extended:true}));
@@ -19,6 +20,7 @@ app.set('view engine','pug');
 app.set('views','./views');
 app.use(express.static('public'));
 app.use('/auth',usuarioRoutes);
+app.use('/',propiedadesRoutes);
 
 const port =3000;
 app.listen(port, ()  => {
